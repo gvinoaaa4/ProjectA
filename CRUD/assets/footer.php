d14014b2d2c510ed2a2870cac3650485a51084d4
gvinoaaa4 <gv.kumar79@gmail.com>
2015-07-22 14:32:31 -0700

    .

---
+++ CRUD/assets/footer.php
@@ -0,0 +1,9 @@
+
+<div class="footer">
+<p class="copyright">
+    <?php echo $CRUD['DBVERSION'] ?> &middot; CRUD Version <?php echo VERSION ?><br />
+    Copyright &copy; 2009-2014 The BearHeart Group LLC
+</p>
+
+</body>
+</heml>
---
+++ SID/assets/footer.php
@@ -0,0 +1,13 @@
+
+<div class="footer">
+<p class="copyright">
+    <?php echo $SID['DBVERSION'] ?>
+    &middot;
+    SID Version <?php echo VERSION ?>
+    <br />
+    Copyright &copy; 2009-2014 The BearHeart Group LLC
+</p>
+</div>
+
+</body>
+</html>
